4ff297ab1d073c54b086b8b43572ee301035f025
David Hicks <davidhicks14@gmail.com>
2019-06-07 11:57:57 -0700

    choices disabled until show answer pressed

--- js/endofmode-rq.js
+++ js/endofmode-rq.js
@@ -1,1 +1,13 @@
 var endOfModeStateRQ = Object.create(endOfGameState);
+
+endOfModeStateRQ.isGameOver = function(mindStateGameOver){
+    var winnerFound = false;
+    for (var i = 0; i < game.global.chars.length; i++) {
+      if(game.global.chars[i].numJewels >= 5){
+        winnerFound = true;
+        break;
+      }
+    }
+    mindStateGameOver = false;
+    return (mindStateGameOver || winnerFound);
+  }
